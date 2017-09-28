1b14f72cd16a09d48cddb1d1d0e55681aa550b8a
andygimma <andy.gimma@kiron.ngo>
2017-09-28 01:14:55 -0500

    Add i18n

--- app/controllers/donatable/application_controller.rb
+++ app/controllers/donatable/application_controller.rb
@@ -1,5 +1,17 @@
 module Donatable
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+    before_action :set_locale
+
+    private
+
+    def set_locale
+      if params[:locale]
+        cookies[:locale] = params[:locale]
+        redirect_back(fallback_location: root_path)
+      end
+      cookies[:locale] = params[:locale] || cookies[:locale] || I18n.default_locale
+      I18n.locale = cookies[:locale]
+    end
   end
 end
--- lib/donatable/engine.rb
+++ lib/donatable/engine.rb
@@ -1,6 +1,10 @@
 module Donatable
   class Engine < ::Rails::Engine
     isolate_namespace Donatable
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+      config.i18n.default_locale = :es
+    end
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
