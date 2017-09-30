430fa32704d9ebc2b8debe57df8bf75d7ef887db
andygimma <andy.gimma@kiron.ngo>
2017-09-29 21:44:06 -0500

    Update original migration

--- db/migrate/20170928052215_create_donatable_organizations.rb
+++ db/migrate/20170928052215_create_donatable_organizations.rb
@@ -1,8 +1,10 @@
 class CreateDonatableOrganizations < ActiveRecord::Migration[5.1]
   def change
     create_table :donatable_organizations do |t|
-      t.string :name
-      t.string :website
+      t.string :name, null: false
+      t.string :email
+      t.string :youtube_url
+      t.string :website, null: false
       t.string :twitter
       t.string :facebook
       t.string :phone
@@ -11,8 +13,8 @@ class CreateDonatableOrganizations < ActiveRecord::Migration[5.1]
       t.string :country
       t.string :banner_url
       t.string :logo_url
-      t.string :short_description
-      t.text :long_description
+      t.string :short_description, null: false
+      t.text :long_description, null: false
 
       t.timestamps
     end
