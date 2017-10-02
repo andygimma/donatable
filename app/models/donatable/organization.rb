304fcaab729ed20d478ad9189825b28406905ad7
andygimma <andy.gimma@kiron.ngo>
2017-10-01 19:35:04 -0500

    update language search/friendly_id

--- app/models/donatable/organization.rb
+++ app/models/donatable/organization.rb
@@ -5,7 +5,7 @@ module Donatable
     acts_as_taggable
     acts_as_taggable_on :tags
 
-    friendly_id :name, use: :slugged
+    friendly_id :combine_name_and_language, use: :slugged
     pg_search_scope :public_search,
                     against: [:name, :short_description, :long_description],
                     using: :dmetaphone
@@ -15,13 +15,19 @@ module Donatable
     validates :short_description, presence: true
     validates :long_description, presence: true
 
-    validates_uniqueness_of :name
-    validates_uniqueness_of :website
+    validates_uniqueness_of :name, scope: :language
+    validates_uniqueness_of :website, scope: :language
 
     has_attached_file :main_image, styles: { medium: '300x300>', thumb: '100x100>' }
     validates_attachment_content_type :main_image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
     has_attached_file :logo_image, styles: { logo: '80x80' }
     validates_attachment_content_type :logo_image, content_type: ['image/jpg', 'image/jpeg', 'image/png']
+
+    private
+
+    def combine_name_and_language
+      self.name + '-' + self.language
+    end
   end
 end
